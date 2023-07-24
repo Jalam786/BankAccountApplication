8ffa4002718e3dc4529d3496844531c354f23551
Junaid <junaid.nituk@gmail.com>
2023-07-24 23:09:59 +0530

    first

---
+++ StudentDatabase/src/main/java/studentdatabase/StudentDatabase.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package studentdatabase;
+
+/**
+ *
+ * @author junai
+ */
+public class StudentDatabase {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

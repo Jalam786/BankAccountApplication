f845d2723b2eb9a660efa70b342cb39834539025
Junaid <junaid.nituk@gmail.com>
2023-07-25 23:51:28 +0530

    implemented specific properties to saving account

--- BankAccountApplication/src/main/java/bankaccountapplication/Savings.java
+++ BankAccountApplication/src/main/java/bankaccountapplication/Savings.java
@@ -3,7 +3,8 @@ package bankaccountapplication;
 
 
 public class Savings extends Account {
-    
+    int safetyDepBoxID;
+    int safetyDepBoxKey;
     
     
     
@@ -11,9 +12,14 @@ public class Savings extends Account {
       public Savings(String name,String sSN,double initDeposit)
     {
     super(name,sSN,initDeposit);
+    // properties specific to saving account
     accountNumber ="1" + accountNumber;
+    setSafetyBox();
     
 }
-
-    
+// method specific to saving account
+    private void setSafetyBox(){
+        safetyDepBoxID = (int) (Math.random() * Math.pow(10,3));
+        safetyDepBoxKey= (int) (Math.random() * Math.pow(10,4));
+    }
 }
